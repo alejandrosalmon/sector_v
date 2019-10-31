7f0a5bd7e9bbe1f171655c277c19124fd797b702
miguelxngx <A01350836@itesm.mx>
2019-10-31 17:27:53 -0600

    register entry button and entries left text

--- sectorV/src/context/EntryContext.js
+++ sectorV/src/context/EntryContext.js
@@ -5,6 +5,10 @@ const entryReducer=(state,action)=>{
     switch(action.type){
         case 'fetch_entries':
             return action.payload;
+        case 'fetch_entries_month':
+            return action.payload;
+        case 'register_entry':
+            return action.payload;
         default: return state;
     }
 };
@@ -14,8 +18,18 @@ const fetchEntries = dispatch =>async() =>{
     dispatch({type: 'fetch_entries',payload: response.data});
 };
 
+const fetchEntriesMonth = dispatch => async() =>{
+    const response = await expressAPI.get('/entries_this_month');
+    dispatch({type: 'fetch_entries_month', payload:response.data});
+}
+
+const registerEntry = dispatch => async() =>{
+    const response = await expressAPI.post('/entries');
+    dispatch({type: 'register_entry', payload:response.data});
+}
+
 export const {Provider,Context}=createDataContext(
     entryReducer,
-    {fetchEntries},
+    {fetchEntries, fetchEntriesMonth, registerEntry},
     []
 );
--- sectorV/src/screens/CodeBarScreen.js
+++ sectorV/src/screens/CodeBarScreen.js
@@ -1,18 +1,54 @@
 import React, {useContext} from 'react';
-import {View,StyleSheet} from 'react-native';
+import {View,StyleSheet,Button} from 'react-native';
 // import {Button} from 'react-native-elements';
 // import Spacer from '../components/Spacer';
 // import {Context as AuthContext} from '../context/AuthContext';
-import {SafeAreaView} from 'react-navigation';
-import {Text} from 'react-native-elements';
+import {SafeAreaView, NavigationEvents} from 'react-navigation';
+import {Text, Image} from 'react-native-elements';
+import Spacer from '../components/Spacer';
 import {Ionicons} from '@expo/vector-icons';
 import {Context as EntryContext} from '../context/EntryContext';
+import {Context as ProfileContext} from '../context/ProfileContext';
+import {Context as PackageContext} from '../context/PackageContext';
 
 const CodeBarScreen = ()=>{
-    // const {signout} = useContext(AuthContext);
+    const {state:userState, fetchProfiles}=useContext(ProfileContext);
+    const {state:entryState, fetchEntriesMonth, registerEntry}=useContext(EntryContext);
+    const {state:packageState, fetchPackage}=useContext(PackageContext);
+    //const leftEntries = Object.keys(entry).length;
     return(
         <SafeAreaView forceInset={{top:'always'}}>
-            <Text h3>Inicio</Text>
+            <NavigationEvents onWillFocus={()=>{
+                fetchProfiles();
+                fetchPackage();
+                fetchEntriesMonth();
+            }}/>
+            <Spacer>
+                <Text h3>Inicio</Text>
+            </Spacer>
+            <Spacer>
+                <Text style={styles.name}>{userState.name}</Text>
+            </Spacer>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: '../../assets/codebar.png' }}
+                    style={styles.image}
+                />
+                <Text>{userState.credential_id}</Text>
+            </View>
+            <Spacer>
+                <Text h4>Día de corte: {userState.due_date}</Text>
+                <Text h4>Entradas Restantes: {packageState.entries_per_month - entryState.length}</Text>
+            </Spacer>
+            <Spacer>
+                <Button
+                    title = "Registrar entrada"
+                    onPress = {()=>{
+                        registerEntry();
+                        fetchEntriesMonth();
+                    }}
+                />
+            </Spacer>
         </SafeAreaView>
     );
 };
@@ -20,5 +56,19 @@ CodeBarScreen.navigationOptions ={
     title: 'Inicio',
     tabBarIcon: <Ionicons name="ios-barcode" size={20}/>
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    name: {
+        textAlign: 'center', // <-- the magic
+        fontWeight: 'bold',
+        fontSize: 36
+    },
+    image:{
+        width: 350,
+        height: 150
+    },
+    container:{
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
 export default CodeBarScreen;

f6d44d8e46a7c3a57adbfaa3fc183377c3280377
97joaquinhr <97joaquinhr@gmail.com>
2019-11-16 12:04:35 -0600

    Added state for data, copy of userlist

--- sectorV/src/screens/UserListScreen.js
+++ sectorV/src/screens/UserListScreen.js
@@ -1,15 +1,24 @@
-import React,{useContext} from 'react';
+import React,{useContext, useState} from 'react';
 import {Text,StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import {NavigationEvents} from 'react-navigation';
 import { Context as UserContext } from "../context/UserContext";
-import {ListItem} from 'react-native-elements';
+import {ListItem, SearchBar} from 'react-native-elements';
+
 
 const UserListScreen = ({navigation})=>{
+    const [search, setSearch] = useState('');
+    const [data, setData] = useState([]);
     const {state, fetchUsers}=useContext(UserContext);
+
+    
+
     return <>
-        <NavigationEvents onWillFocus={fetchUsers}/>
+        <NavigationEvents onWillFocus={()=>{
+            fetchUsers();
+            setData(state);
+            }}/>
         <FlatList
-            data ={state}
+            data ={data}
             keyExtractor={item=>item._id}
             renderItem = {({item,index})=>{
                 return <TouchableOpacity

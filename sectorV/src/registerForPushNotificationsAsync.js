031c0ba1ebfd14bee0f140f098fb72a477798fa8
97joaquinhr <97joaquinhr@gmail.com>
2019-11-21 13:10:42 -0600

    Push notification working

---
+++ sectorV/src/registerForPushNotificationsAsync.js
@@ -0,0 +1,30 @@
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import expressAPI from './api/expressAPI';
+
+export default async function registerForPushNotificationsAsync() {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== 'granted') {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+  // POST the token to your backend server from where you can retrieve it to send push notifications.
+  const response = await expressAPI.post('/push-token',{token});
+  return response;
+}

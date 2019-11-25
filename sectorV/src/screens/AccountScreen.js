5023f337c526647e32008c13f1e0a69fe0f6a37b
97joaquinhr <97joaquinhr@gmail.com>
2019-11-24 19:06:00 -0600

    fixed accountscreen

--- sectorV/src/screens/AccountScreen.js
+++ sectorV/src/screens/AccountScreen.js
@@ -7,25 +7,26 @@ import {SafeAreaView} from 'react-navigation';
 import {Feather} from '@expo/vector-icons';
 import { Context as ProfileContext } from "../context/ProfileContext";
 import {NavigationEvents} from 'react-navigation';
-import {ScrollView} from 'react-native';
-import {Ionicons, MaterialIcons} from '@expo/vector-icons';
 
 const AccountScreen = ()=>{
     const {signout} = useContext(AuthContext);
     const {state, fetchProfiles}=useContext(ProfileContext);
     return(
-        <View style={styles.infoContainer}>
-                <Text h3> {state.name} </Text>
-                <Text h4> {state.email} </Text>
-                <Text h4> {state.role == '0'?'Admin':"Alumno"}</Text>
-                <Text h4> Fecha de Registro </Text>
-                <Text style={{fontSize: 18}}> {state.registration_time} </Text>
-
-                <Button
-                    title="Cerrar sesión"
-                    onPress={signout}
-                />
-        </View>
+        <SafeAreaView forceInset={{top:'always'}} style={styles.infoContainer}>
+            <NavigationEvents onWillFocus={fetchProfiles}/>
+            <Text h3> {state.name} </Text>
+            <Text h4> {state.email} </Text>
+            <Text h4> {state.role == '0'?'Admin':"Alumno"}</Text>
+            <Spacer/>
+            <Text h4> Fecha de Registro </Text>
+            <Text style={{fontSize: 18}}> {state.registration_time} </Text>
+            
+            <Spacer/>
+            <Button
+                title="Cerrar sesión"
+                onPress={signout}
+            />
+        </SafeAreaView>
     
     );
 };
